bb069371fd3602a8f6d355f597c4809d8ea55dba
Blessing Victor <blessingvictor998@gmail.com>
2023-11-28 09:01:37 -0800

    Python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    p = ""
-    for s in range(len(str)):
-        if p != n:
-            p = p + str[s]
-    return (p)
+  if 0 <= n < len(str):
+      return str[:n] + str[n + 1:]
+  else:
+      return s
+
+# Example usage:
+original_string = "chicago"
+new_string = remove_char_at(original_string, 3)
+print("Original String:", original_string)
+print("String after removing character at position 0:", new_string)
