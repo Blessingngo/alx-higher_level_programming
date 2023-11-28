b2de4cb17f55d5fc2ad387dda39d2e249cb6593b
Blessing Victor <blessingvictor998@gmail.com>
2023-11-28 08:05:04 -0800

    Python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-  last_digit = number % 10
+  last_digit = abs(number) % 10  # Use abs to handle negative numbers
   print(last_digit)
   return last_digit
 
 # Example usage:
-number = 12345
-result = print_last_digit(number)
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
