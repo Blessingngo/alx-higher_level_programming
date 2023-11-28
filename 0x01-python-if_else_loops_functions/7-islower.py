3036186916d7debb18f8c1dc833fde511cec68f3
Blessing Victor <blessingvictor998@gmail.com>
2023-11-28 07:41:42 -0800

    Python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-character = 'a'
-result = islower(character)
-print(result)
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
+    else:
+        return (False)
