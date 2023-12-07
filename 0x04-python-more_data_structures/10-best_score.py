5d19da93a0f7e94379f8842ed83df024ec6088c9
Blessing Victor <blessingvictor998@gmail.com>
2023-12-06 18:20:12 -0800

    Python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
+    if not a_dictionary:
+        return (None)
 
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for l, m in a_dictionary.items():
-        if m > big:
-            big = m
-            ret = l
-    return (ret)
+    return (max(a_dictionary, key=a_dictionary.get))
