c0922d8309e4b092d073ee3c96570d03254734ff
Blessing Victor <blessingvictor998@gmail.com>
2023-12-06 17:42:35 -0800

    Python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <Python.h>
 
 /**
  * print_python_list_info - prints python list info
@@ -7,7 +6,7 @@
  * @p: PyObject
  * Return: no return
  */
-void print_python_list_info(PyObject *p)
+void print_python_list_info(PyObject *p);
 {
 	long int size, i;
 	PyListObject *list;
